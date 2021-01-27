c0fe407512345fd381ff17c49c8bb6e09387236b
manuel borowski <emmanuel.borowski@gmail.com>
2021-01-27 23:15:14 +0100

    V0.3 : update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -16,10 +16,11 @@ flask_app = Flask(__name__, instance_relative_config=True, template_folder='pres
 
 # V0.1 : start from infodemol 0.7
 # V0.2 : registration ok, ack e-mail is sent
+# V0.3 : update requirements.txt
 
 @flask_app.context_processor
 def inject_version():
-    return dict(version='V0.2')
+    return dict(version='V0.3')
 
 #enable logging
 LOG_HANDLE = 'DSB'
